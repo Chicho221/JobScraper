9bde96abc1a83417acd826d6b471e3e863654fcd
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-23 23:00:42 +0100

    Add main logic

---
+++ main.py
@@ -0,0 +1,10 @@
+from scraper import fetch_jobs
+
+def main():
+    jobs = fetch_jobs
+
+    for i, job in enumerate(jobs, 1):
+        print(f"{i}. {job['title']} - {job['company']}")
+
+if __name__ == "__main__":
+    main()

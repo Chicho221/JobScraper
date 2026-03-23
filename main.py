c5aa0fef4f20b4e60294d725aaf9c98ff86774b8
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-24 00:02:42 +0100

    Add timestamp function

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from scraper import fetch_jobs
+from datetime import datetime
 import json
 
 def main():
+
+    #Takes timestamp and formats it    
+    def timestamp():
+        time = datetime.now()
+        formatted_time = time.strftime("%Y-%m-%d %H:%M")
+        return formatted_time
+    
     jobs = fetch_jobs()
     for i, job in enumerate(jobs, 1):
-        print(f"{i}. {job['title']} - {job['company']}")
+        print(f"{i}. {job['title']} - {job['company']}")   
+    print(f"\nScraped at: {timestamp()}\n")
 
     #Saves in JSON file
     with open("JobScraper/jobs.json", "w") as file:
@@ -18,6 +27,6 @@ def main():
         #Prints all jobs with keyward entered
         for job in filtered:
             print(f"{job['title']} - {job['company']}")
-
+        print(f"\nFiltered at: {timestamp()}\n")
 if __name__ == "__main__":
     main()

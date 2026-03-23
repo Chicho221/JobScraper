c4cd50a1387d3f57ab6e922af8ef75f13bf57d07
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-23 23:32:45 +0100

    Add search by keyword

--- main.py
+++ main.py
@@ -6,7 +6,18 @@ def main():
     for i, job in enumerate(jobs, 1):
         print(f"{i}. {job['title']} - {job['company']}")
 
+    #Saves in JSON file
     with open("JobScraper/jobs.json", "w") as file:
         json.dump(jobs, file, indent=4)
+
+    #Asks for keyword and filters array
+    while True:
+        keyword = input("Enter keyword: ").lower()
+        filtered = [job for job in jobs if keyword in job["title"].lower()]
+
+        #Prints all jobs with keyward entered
+        for job in filtered:
+            print(f"{job['title']} - {job['company']}")
+
 if __name__ == "__main__":
     main()

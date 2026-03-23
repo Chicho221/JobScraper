1ba80a3b6e562e3b54d2403a81362be7b0b49eb7
Chicho221 <135489598+Chicho221@users.noreply.github.com>
2026-03-23 23:17:45 +0100

    Add JSON file generation with saved data

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from scraper import fetch_jobs
+import json
 
 def main():
     jobs = fetch_jobs()
     for i, job in enumerate(jobs, 1):
         print(f"{i}. {job['title']} - {job['company']}")
 
+    with open("JobScraper/jobs.json", "w") as file:
+        json.dump(jobs, file, indent=4)
 if __name__ == "__main__":
     main()
